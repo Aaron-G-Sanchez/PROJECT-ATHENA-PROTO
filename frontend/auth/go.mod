32d826a03cd52d119ede7dab93199237fe8099e8
Aaron Sanchez <saarong12@gmail.com>
2025-02-13 22:34:17 -0600

    Added ZITADEL auth prototype.

--- frontend/auth/go.mod
+++ frontend/auth/go.mod
@@ -2,4 +2,29 @@ module github.com/aaron-g-sanchez/PROTOTYPE/PROJECT-ATHENA-PROTO/frontend/auth
 
 go 1.23.2
 
-require github.com/a-h/templ v0.3.833 // indirect
+require (
+	github.com/a-h/templ v0.3.833
+	github.com/zitadel/zitadel-go/v3 v3.4.1
+)
+
+require (
+	github.com/go-jose/go-jose/v4 v4.0.4 // indirect
+	github.com/go-logr/logr v1.4.2 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/gorilla/securecookie v1.1.2 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/muhlemmer/gu v0.3.1 // indirect
+	github.com/sirupsen/logrus v1.9.3 // indirect
+	github.com/zitadel/logging v0.6.1 // indirect
+	github.com/zitadel/oidc/v3 v3.33.1 // indirect
+	github.com/zitadel/schema v1.3.0 // indirect
+	go.opentelemetry.io/otel v1.29.0 // indirect
+	go.opentelemetry.io/otel/metric v1.29.0 // indirect
+	go.opentelemetry.io/otel/trace v1.29.0 // indirect
+	golang.org/x/crypto v0.31.0 // indirect
+	golang.org/x/exp v0.0.0-20240719175910-8a7402abbf56 // indirect
+	golang.org/x/oauth2 v0.24.0 // indirect
+	golang.org/x/sys v0.28.0 // indirect
+	golang.org/x/text v0.21.0 // indirect
+)
--- frontend/auth/main.go
+++ frontend/auth/main.go
@@ -1,18 +1,72 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
+	"os"
 
-	"github.com/a-h/templ"
 	"github.com/aaron-g-sanchez/PROTOTYPE/PROJECT-ATHENA-PROTO/frontend/auth/templates"
+	"github.com/joho/godotenv"
+	"github.com/zitadel/zitadel-go/v3/pkg/authentication"
+	openid "github.com/zitadel/zitadel-go/v3/pkg/authentication/oidc"
+	"github.com/zitadel/zitadel-go/v3/pkg/zitadel"
 )
 
-func main() {
-	component := templates.Home()
+var (
+	domain      string
+	key         string
+	clientId    string
+	redirectUri string
+)
 
-	http.Handle("/", templ.Handler(component))
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("error loading env", err)
+	}
+
+	domain = os.Getenv("DOMAIN")
+	key = os.Getenv("KEY")
+	clientId = os.Getenv("CLIENT_ID")
+	redirectUri = os.Getenv("REDIRECT_URI")
+}
+
+func main() {
+
+	ctx := context.Background()
+
+	authN, err := authentication.New(ctx, zitadel.New(domain), key, openid.DefaultAuthentication(clientId, redirectUri, key))
+	if err != nil {
+		log.Fatal("zitadel sdk could not be initialized", err)
+	}
+
+	mw := authentication.Middleware(authN)
+
+	router := http.NewServeMux()
+
+	router.Handle("/auth/", authN)
+
+	router.Handle("/profile", mw.RequireAuthentication()(http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		authCtx := mw.Context(req.Context())
+		data, err := json.MarshalIndent(authCtx.UserInfo, "", "")
+		if err != nil {
+			log.Fatal("error marshalling profile response", err)
+		}
+		component := templates.Profile(string(data))
+		component.Render(req.Context(), w)
+	})))
+
+	router.Handle("/", mw.CheckAuthentication()(http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		if authentication.IsAuthenticated(req.Context()) {
+			http.Redirect(w, req, "/profile", http.StatusFound)
+		}
+		homeComponent := templates.Home()
+		homeComponent.Render(req.Context(), w)
+	})))
 
 	fmt.Println("Listening on :3000")
-	http.ListenAndServe(":3000", nil)
+	http.ListenAndServe(":3000", router)
 }
